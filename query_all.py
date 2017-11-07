e51159cdc3d261def5d16bb50beb689d4d648e1c
=epogrebnyak <e.pogrebnyak@gmail.com>
2017-11-07 23:44:10 +0300

    moved files from 'visuals' repo

---
+++ query_all.py
@@ -0,0 +1,72 @@
+"""Load test: use 'api/names' to get listing of all variables and download it.
+
+~ 27 000 observations,  
+
+{'a': {'len': 524, 'ts': 29},
+ 'd': {'len': 16259, 'ts': 13},
+ 'm': {'len': 7131, 'ts': 33},
+ 'q': {'len': 2515, 'ts': 35}}
+
+NOT IMPLEMENTED:
+
+   - map of dataset (start and end dates oa grid)    
+
+"""
+
+import requests 
+
+class Frequency:
+    
+    def __init__(self, freq):
+        self.freq = freq
+        
+    @property    
+    def url(self):
+        return f'https://minikep-db.herokuapp.com/api/names/{self.freq}'
+    
+    @property    
+    def names(self):
+        return requests.get(self.url).json()
+
+class Datapoints:
+    
+    def __init__(self, freq, name):
+        self.freq = freq
+        self.name = name
+    
+    @property    
+    def url(self):
+        return ( 'https://minikep-db.herokuapp.com/api/datapoints'
+                f'?freq={self.freq}&name={self.name}&format=json')  
+    @property    
+    def data(self):
+        return requests.get(self.url).json()
+
+if __name__ == '__main__':
+    from time import time
+
+    div = {'a': 1,
+     'd': 1/(52*5),
+     'm': 1/12,
+     'q': .25}    
+    
+    start = time()
+    
+    u = {}   
+    for freq in 'aqmd':    
+        q = []
+        for i, name in enumerate(Frequency(freq).names):
+            print(freq, name)
+            q.extend(Datapoints(freq, name).data)        
+        u[freq] = dict(ts=i, len=len(q))    
+        
+
+    elapsed = time() - start     
+ 
+    for freq in 'aqmd':    
+        print (freq, u[freq]['len'] / u[freq]['ts'] * div[freq])
+    
+    print(elapsed)
+    
+    d = Datapoints('a', 'GDP_yoy')
+    
---
+++ viz.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def make_url(freq, name):    
+    return ('https://minikep-db.herokuapp.com/api/'
+           f'datapoints?name={name}&freq={freq}')
+
+
+def read_from_url(source_url):
+	"""Read pandas time series from *source_url*."""
+	return pd.read_csv(source_url, 
+                       converters={0: pd.to_datetime}, 
+                       index_col=0,
+                       squeeze=True)
+
+def get_ts(freq, name):
+    url = make_url(freq, name)
+    return read_from_url(url)
+
+def get_df(freq, *names):
+    df_list = [get_ts(freq, name).to_frame() for name in names]
+    df = df_list[0]
+    for right_df in df_list[1:]:
+       df = df.merge(right_df, right_index=True, left_index=True)
+    return df  
+
+df = get_df('a', 'GDP_yoy', 'CPI_rog')  
+assert isinstance(df, pd.DataFrame)
+
+# this fails
+# a = ['GDP_yoy', 'INDPRO_rog', 'INVESTMENT_rog']
+
+
+start = datetime.date(1998, 12, 31)
+end = datetime.date(2017, 12, 31)
+DEFAULT_TIMERANGE = start, end
+
+# graphics parameters in dicts
+
+SPLINE_GPARAMS = {'timerange': DEFAULT_TIMERANGE,
+                  'figsize': (2, 0.6),
+                  'style': 'ggplot',
+                  'facecolor': 'white',
+                  'auto_x': False,
+                  'axis_on': False}
+
+INDICATOR_GPARAMS = {'timerange': DEFAULT_TIMERANGE,
+                     'figsize': (5, 5),
+                     'style': 'bmh',
+                     'facecolor': 'white',
+                     'auto_x': False,
+                     'axis_on': True}
+
+def get_frequency(ts):
+    """Returns the frequency of a timeseries or dataframe.
+        Args:
+            ts: pd.Timeseries
+        Returns:
+            string
+    """
+    return str.lower(pd.infer_freq(ts.index))
+
+
+class GraphBase:
+    """Parent class for project charts (Spline, Chart and other)."""
+
+    def __init__(self, ts, params={}):
+        """
+        Args:
+            ts (pd.TimeSeries):
+            params (dict): chart formattingstyle parameters
+        """
+        self.ts = ts
+        self.params = params
+        # nothing plotted yet
+        self.fig = None
+
+    def plot(self):
+        plt.style.use(self.params['style'])
+        # create figure
+        fig = plt.figure(figsize=self.params['figsize'])
+        # add 1 subplot and format it
+        axes = fig.add_subplot(1, 1, 1, facecolor=self.params['facecolor'])
+        axes.set_xlim(self.params['timerange'])
+        # draw data at axis
+        axes.plot(self.ts)
+        # format figure
+        if self.params['auto_x']:
+            fig.autofmt_xdate()
+        if not self.params['axis_on']:
+            plt.axis('off')
+        self.fig = fig
+        return self
+
+    def close(self):
+        plt.close()
+        self.fig = None
+
+
+class Spline(GraphBase):
+    def __init__(self, ts):
+        super().__init__(ts, params=SPLINE_GPARAMS)
+
+class Chart(GraphBase):
+    def __init__(self, df, title=None):
+        super().__init__(df, params=INDICATOR_GPARAMS)
+
+
+class ChartStack(GraphBase):
+    def __init__(self, df, name=None):
+        super().__init__(df, params=INDICATOR_GPARAMS)
+
+    def plot(self):
+        plt.style.use(self.params['style'])
+        fig = plt.figure(figsize=self.params['figsize'])
+        num_plots = len(self.ts.columns)
+        for i in range(num_plots):
+            ax = fig.add_subplot(num_plots, 1, i + 1)
+            ax.plot(df.iloc[:, i])
+
+
+if __name__ == "__main__":
+
+    ts = get_ts('q', 'CPI_rog')
+    s = Spline(ts)
+    c = Chart(ts)
+
+    s.plot()
+    c.plot()
+
+    varnames = ['RETAIL_SALES_FOOD_bln_rub',
+                'RETAIL_SALES_NONFOOD_bln_rub',
+                'RETAIL_SALES_bln_rub'
+                ]
+    df = pd.concat([get_ts('q', name) for name in varnames], axis=1,
+            keys=varnames)
+    v = ChartStack(df)
+    v.plot()
+
+    # ----------------------------------------
+    
+    qv = ['GDP_rog'
+          'INDPRO_rog',
+          'INVESTMENT_rog']
+
+    mv = ['INDPRO_yoy',
+          'TRANSPORT_FREIGHT_bln_tkm',
+          'CPI_rog',
+          'WAGE_REAL_rog',
+          'UNEMPL_pct',
+          'GOV_SURPLUS_ACCUM_FEDERAL_bln_rub',
+          'EXPORT_GOODS_bln_usd',
+          'IMPORT_GOODS_bln_usd']
+
+   # ----------------------------------------
+    
