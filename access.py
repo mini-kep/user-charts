557c1d65753c1f11be77fde5ae7d8ae9a103dfa8
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-11-10 10:34:14 +0300

    Update access.py

--- access.py
+++ access.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-
 import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 def make_url(freq, name):    
-    return ('https://minikep-db.herokuapp.com/api/'
-           f'datapoints?name={name}&freq={freq}')
+    return ('https://minikep-db.herokuapp.com/api/datapoints'
+            '?name={}&freq={}'.format(name, freq))
 
 
 def read_from_url(source_url):
@@ -25,8 +23,13 @@ def get_df(freq, *names):
     df_list = [get_ts(freq, name).to_frame() for name in names]
     df = df_list[0]
     for right_df in df_list[1:]:
-       df = df.merge(right_df, right_index=True, left_index=True)
+	# inner join, restrictive if you do not have some data, can change to outer
+        df = df.merge(right_df, right_index=True, left_index=True)
     return df  
 
-df = get_df('a', 'GDP_yoy', 'CPI_rog')  
-assert isinstance(df, pd.DataFrame
+if __name__ == '__main__':
+    ts = get_df('a', 'GDP_yoy') 	
+    assert isinstance(df, pd.Series)	
+    df = get_df('a', ['GDP_yoy', 'CPI_rog'])  
+    assert isinstance(df, pd.DataFrame)
+	
