c3129fa141d3a084769f22df81170b00788d2efb
=epogrebnyak <e.pogrebnyak@gmail.com>
2017-11-16 00:47:30 +0300

    local files for #1

--- access.py
+++ access.py
@@ -1,5 +1,8 @@
+import os
 import requests
 import pandas as pd
+from time import time
+    
 
 def fetch_json(url):
     return requests.get(url).json()    
@@ -19,14 +22,20 @@ def make_url(freq, name, format):
 def get_datapoints(freq, name):
     url = make_url(freq, name, 'json') 
     return fetch_json(url)
-    
-def read_from_url(url):
-    """Read pandas time series from *source_url*."""
-    return pd.read_csv(url, converters={0: pd.to_datetime}, index_col=0, squeeze=True)
+
+set_index = dict(converters={0: pd.to_datetime}, index_col=0)  
+
+def read_ts(file_or_url):
+    """Read pandas time series from *file_or_url*."""
+    return pd.read_csv(file_or_url, **set_index, squeeze=True)
+
+def read_df(file_or_url):
+    """Read pandas dataframe from *file_or_url*."""
+    return pd.read_csv(file_or_url, **set_index)
  
 def get_ts(freq, name):  
     url = make_url(freq, name, 'csv')
-    return read_from_url(url)
+    return read_ts(url)
     
 def get_df_by_names(freq, names):
     df_list = [get_ts(freq, name).to_frame() for name in names]
@@ -39,19 +48,37 @@ def get_df(freq):
     names = get_names(freq)
     return get_df_by_names(freq, names)
 
-# TODO: save data in csv file as backups, similar as kep parser does it. 
+def make_filename(freq):
+    return os.path.join('data', f'df{freq}.csv')
 
+def save_local(freq):
+    df = get_df(freq) 
+    fn = make_filename(freq)
+    df.to_csv(fn) 
+
+def get_df_local(freq):  
+    path = make_filename(freq)
+    return read_df(path)
+    
 if __name__ == '__main__':
     # get variable list for frequency 'q' (quarterly)
     variable_names_quarterly = get_names('q')
+    assert 'GDP_yoy' in variable_names_quarterly 
+    
     # read one variable as pd.Series
     ts = get_ts('q', 'GDP_yoy')
+
     # read all variables for frequency 'q' as pd.DataFrame 
     # runs about 20-40 sec
     dfq = get_df('q')    
-    # check dataframe columns are exaactly the ones we retrieved earlier
+    # check dataframe columns are exactly the ones we retrieved earlier
     assert variable_names_quarterly == dfq.columns.tolist()    
+
+    # update local files (about one minute on my machine) 
+    start = time()
+    for freq in 'aqmd':
+        save_local(freq) 
+    # reading a local version
+    print("Read full dataset:", round(time() - start, 1), "sec")
     
-    # can also get monthly data
-    # commented because it slows down code, uncomment if you need monthly data
-    # dfm = get_df('m')    
+    
