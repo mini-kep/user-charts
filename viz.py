a4bcbce49e8d2e9c6dd395c1e6156e32cc77f78e
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-11-16 13:16:07 +0300

    Update viz.py

--- viz.py
+++ viz.py
@@ -1,39 +1,18 @@
 # -*- coding: utf-8 -*-
+#
+#  See also visualisations in: 
+#     https://github.com/mini-kep/parser-rosstat-kep/issues/78
+#
+
 
 import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-def make_url(freq, name):    
-    return ('https://minikep-db.herokuapp.com/api/'
-           f'datapoints?name={name}&freq={freq}')
-
-
-def read_from_url(source_url):
-	"""Read pandas time series from *source_url*."""
-	return pd.read_csv(source_url, 
-                       converters={0: pd.to_datetime}, 
-                       index_col=0,
-                       squeeze=True)
-
-def get_ts(freq, name):
-    url = make_url(freq, name)
-    return read_from_url(url)
-
-def get_df(freq, *names):
-    df_list = [get_ts(freq, name).to_frame() for name in names]
-    df = df_list[0]
-    for right_df in df_list[1:]:
-       df = df.merge(right_df, right_index=True, left_index=True)
-    return df  
+from access import get_ts, get_df
 
 df = get_df('a', 'GDP_yoy', 'CPI_rog')  
 assert isinstance(df, pd.DataFrame)
-
-# this fails
-# a = ['GDP_yoy', 'INDPRO_rog', 'INVESTMENT_rog']
-
 
 start = datetime.date(1998, 12, 31)
 end = datetime.date(2017, 12, 31)
